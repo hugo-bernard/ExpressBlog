60fca2de99b603273bfb54bb927a5323143cd071
Hugo Bernard <hugo.bernard@epitech.eu>
2023-04-11 18:23:52 +0100

    fix useless import

--- app/src/views/Article.js
+++ app/src/views/Article.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navigation from '../components/Navigation'
 import { useLocation } from 'react-router-dom'
 import axios from 'axios';
