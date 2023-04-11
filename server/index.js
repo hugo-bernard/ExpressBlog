12548fa72f3f75f56d94f3bd2baf1b812ab18389
Hugo Bernard <hugo.bernard@epitech.eu>
2023-04-11 17:54:32 +0100

    add root package.json

--- server/index.js
+++ server/index.js
@@ -23,8 +23,9 @@ app.use(cors())
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
 })
 
 app.use('/api', routes)
